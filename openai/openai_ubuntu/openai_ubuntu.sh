19416cc86b31c7a20d42a9d8967dde7be2517b4e
ronrest <ronny.coding@gmail.com>
2017-01-11 21:35:10 +1100

    OPENAI - setup virtualenv

--- openai/openai_ubuntu/openai_ubuntu.sh
+++ openai/openai_ubuntu/openai_ubuntu.sh
@@ -2,6 +2,16 @@
 
 # terminate script if any line returns a non-zero exit status
 set -e
+
+#-------------------------------------------------------------------------------
+#                                                                      VARIABLES
+#-------------------------------------------------------------------------------
+# Modify these variables to suit your needs.
+
+PYTHON_VERSION="2.7"             # Change this to 3.5 if desired.
+VIRTUAL_ENV_NAME="openai"        # Name you want to give your virtualenv
+VIRTUAL_ENV_ROOT="~/virtualenvs" # Where your virtual envs are stored.
+                                 # NOTE: no trailing forward slash at the end
 echo "==========================================================="
 echo "                            INSTALL THE DEVELOPER LIBRARIES"
 echo "==========================================================="
@@ -13,5 +23,18 @@ sudo apt-get install -y unzip
 
 # Needed for Open AI
 sudo apt-get install -y golang libjpeg-turbo8-dev make
+
+
+echo "==========================================================="
+echo "                                    SETUP OPENAI VIRTUALENV"
+echo "==========================================================="
+echo "CREATING VIRTUALENV"
+mkdir -p ${VIRTUAL_ENV_ROOT}
+cd ${VIRTUAL_ENV_ROOT}
+virtualenv -p /usr/bin/python${PYTHON_VERSION} ${VIRTUAL_ENV_NAME}
+
+echo "ENTERING VIRTUALENV"
+. ${VIRTUAL_ENV_ROOT}/${VIRTUAL_ENV_NAME}/bin/activate
+
 #
 #
